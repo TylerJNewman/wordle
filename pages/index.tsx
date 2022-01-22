c1ad1e1b165d627291478ef297ebb53e0695af50
Tyler Newman <Tylerjnewman18@gmail.com>
2022-01-21 17:19:39 -0800

    Add ability to enter words

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import {useKey} from 'rooks'
+
 import Board from 'components/Board'
 import Keyboard from 'components/Keyboard'
 import Layout from 'components/Layout'
+import {useState} from 'react'
 
 const deleteSvg = <img src="/delete.svg" alt="delete icon" />
 const qwerty = 'qwertyuiopasdfghjklzxcvbnm'.split('')
@@ -9,9 +12,42 @@ const middleRow = qwerty.slice(9, 18)
 const bottomRow = ['Enter', ...qwerty.slice(18), deleteSvg]
 const keyboard = [topRow, middleRow, bottomRow]
 const generateRow = () => Array(5).fill(null)
-const board = Array(6).fill(generateRow())
+const initialBoard = Array(6).fill(generateRow())
 
 export default function Home() {
+  const [word, setWord] = useState('')
+  const [board, setBoard] = useState(initialBoard)
+  const [rowIndex, setRowIndex] = useState(0)
+
+  const addLetter = ({key}: {key: string}) => {
+    if (word.length < 5) {
+      const newWord = word + key
+      setWord(newWord)
+      updateBoard(newWord)
+    }
+  }
+
+  const deleteLetter = () => {
+    if (word.length > 0) {
+      const newWord = word.slice(0, word.length - 1)
+      setWord(newWord)
+      updateBoard(newWord)
+    }
+  }
+
+  const updateBoard = (newWord: string) => {
+    const newBoard = [...board]
+    const updatedRow = board[rowIndex].map((el: null | string, i: number) =>
+      newWord[i] ? newWord[i] : null,
+    )
+    newBoard[rowIndex] = updatedRow
+    setBoard(newBoard)
+  }
+
+  useKey(qwerty, addLetter)
+  useKey(['Backspace'], deleteLetter)
+  useKey(['Enter'], () => {})
+
   return (
     <Layout>
       <Board board={board} />
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -16,11 +16,15 @@ const Board = ({board}: Props) => {
             key={`row-${i}`}
             className="row grid grid-cols-5 gap-[5px] font-mono text-white text-sm text-center font-bold leading-6 bg-stripes-fuchsia rounded-lg"
           >
-            {row.map((_: any, i) => (
+            {row.map((letter: string | null, i) => (
               <div
                 key={`col-${i}`}
-                className="tile border-2 border-gray-300"
-              ></div>
+                className={`tile flex items-center justify-center text-5xl ${
+                  letter ? 'bg-gray-500' : 'border-2 border-gray-300'
+                } `}
+              >
+                {letter?.toUpperCase()}
+              </div>
             ))}
           </div>
         ))}
